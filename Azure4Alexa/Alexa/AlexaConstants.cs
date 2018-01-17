8085100e74bf61f6e7c7221bb308309c13b00f4d
Tam H <tamhinsf>
2018-01-17 04:56:04 +0000

    Move AppId and AppName configuration variables into Web.config (finally!)

--- Azure4Alexa/Alexa/AlexaConstants.cs
+++ Azure4Alexa/Alexa/AlexaConstants.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace Azure4Alexa.Alexa
 {
@@ -10,10 +11,10 @@ namespace Azure4Alexa.Alexa
         // Inbound requests from Amazon include the Voice Skills AppId, assigned to you when registering your skill
         // Validate the value against what you registered, to ensure that someone else isn't calling your service.  
 
-        // It's bad practice to include the actual AppId in code,
-        // but we'll do so here as to make life easy for you.
+        // In previous versions of this project, we stored the AppId as a code variable (below)
+        // Now, we'll move the location of this setting into Web.Config
 
-        public static string AppId = "";
+        public static string AppId = WebConfigurationManager.AppSettings["AppId"];
 
         // the value of AppName has no correspondence to what you have registered in Amazon
         // we just store it here because it's useful
@@ -21,7 +22,10 @@ namespace Azure4Alexa.Alexa
         // However, it will appear in the card shown to the user in the Alexa Companion app for iOS or Android
         // You might want to change it
 
-        public static string AppName = "Azure4Alexa";
+        // In previous versions of this project, we stored the AppName as a code variable (below)
+        // Now, we'll move the location of this setting into Web.Config
+
+        public static string AppName = WebConfigurationManager.AppSettings["AppName"];
 
         // standard error message
 
